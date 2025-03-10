742d12e968fb4bfb8553553f319a0734d212610c
Sergio <sergiopdjr@hotmail.com>
2025-03-10 18:51:42 -0300

    testing library

--- myblog/tests/models/test_post.py
+++ myblog/tests/models/test_post.py
@@ -2,11 +2,19 @@ import pytest
 
 from blog.factories import PostFactory 
 
+# @pytest.fixture 
+# def post_published(): 
+#     return PostFactory(title='pytest with factory')
+
+
+# @pytest.mark.django_db 
+# def test_create_published_post(post_published): 
+#     assert post_published.title == 'pytest with factory'
+    
 @pytest.fixture 
 def post_published(): 
-    return PostFactory(title='pytest with factory')
-
+    return PostFactory(title='pytest with factory 02')
 
 @pytest.mark.django_db 
 def test_create_published_post(post_published): 
-    assert post_published.title == 'pytest with factory'
+    assert post_published.title == 'pytest with factory 02'
